d4aab0031f86c0725fa61f3fcc38a7c018a1bca9
Sarah Strange <sarah.strange@availity.com>
2018-02-21 14:59:39 -0500

    fixing routing and errors

--- js/app.js
+++ js/app.js
@@ -1,20 +1,38 @@
-var app = angular.module('groceryListApp', []);
+/**
+ * Created by Thomas on 5/28/2015.
+ */
+var app = angular.module('groceryListApp', ["ngRoute"]);
+
+app.config(function($routeProvider){
+    $routeProvider
+        .when("/",{
+            templateUrl: "views/groceryList.html",
+            controller: "GroceryListItemsController"
+        })
+        .when("/addItem",{
+            templateUrl: "views/addItem.html",
+            controller: "GroceryListItemsController"
+        })
+        .otherwise({
+            redirectTo: "/"
+        })
+});
 
 app.controller("HomeController", ["$scope", function($scope) {
     $scope.appTitle = "Grocery List";
 }]);
 
-
 app.controller("GroceryListItemsController", ["$scope", function($scope){
+
     $scope.groceryItems = [
-        {completed: true, itemName: 'milk', date: '2018-02-10'},
-        {completed: true, itemName: 'cookies', date: '2018-02-12'},
-        {completed: true, itemName: 'ice cream', date: '2018-02-11'},
-        {completed: true, itemName: 'potatoes', date: '2018-02-15'},
-        {completed: true, itemName: 'cereal', date: '2018-02-19'},
-        {completed: true, itemName: 'bread', date: '2018-02-18'},
-        {completed: true, itemName: 'eggs', date: '2018-02-14'},
-        {completed: true, itemName: 'tortillas', date: '2018-02-13'}
+        {completed: true, itemName: 'milk', date: '2018-10-01'},
+        {completed: true, itemName: 'cookies', date: '2018-11-18'},
+        {completed: true, itemName: 'ice cream', date: '2018-09-03'},
+        {completed: true, itemName: 'potatoes', date: '2017-10-22'},
+        {completed: true, itemName: 'cereal', date: '2018-12-03'},
+        {completed: true, itemName: 'bread', date: '2018-05-03'},
+        {completed: true, itemName: 'eggs', date: '2018-06-04'},
+        {completed: true, itemName: 'tortillas', date: '2018-10-04'}
     ]
 
-}])
+}]);
